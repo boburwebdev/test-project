38e9381dd86e45467d264763375faab602a1b259
boburwebdev <boburjon.zokirov.2017@gmail.com>
2023-02-10 11:21:25 +0500

    modified scripts

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let name = "mario";
-console.log(`hello ${name}`)
+let username = "mario";
+
+const greet = username => {
+  console.log("hello " + username);
+}
+
+greet(username);
+
+const bye = username => {
+  console.log("bye " + username);
+}
+
+bye(username);
