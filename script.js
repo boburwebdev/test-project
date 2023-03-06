9a0f30892ceb78e449ddc2f73b5db579a0131bf5
boburwebdev <boburjon.zokirov.2017@gmail.com>
2023-03-06 11:57:50 +0500

    added scripts

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+function sayHi(name) {
+  console.log("Hi " + name);
+}
+
+sayHi("Jon");
+
+function sayHello(name) {
+  console.log("Hello " + name);
+}
+
+sayHello("Robb");
+
